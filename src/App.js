ac46773ff2ccb565242b1702973cc7c08af655b0
zhuaofei <2353536563@qq.com>
2018-11-20 21:04:28 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react'; // 占位符
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
+    // 构造函数
+    constructor(props) {
+        super(props)
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+    }
+
+    handleInputChange(e) {
+        console.log(e.target.value)
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    handleSubmit() {
+        console.log(this.state.inputValue)
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
+    }
+    handleDelete(index) {
+        // immutable
+        console.log(index)
+        const listNew = [...this.state.list]
+        listNew.splice(index, 1)
+        this.setState({
+            list: listNew
+        })
+    }
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <input type="text"
+                           value={this.state.inputValue}
+                           onChange={this.handleInputChange.bind(this)}
+                    />
+                    <button onClick={this.handleSubmit.bind(this)}>提交</button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return <li key={index} onClick={this.handleDelete.bind(this,index)}>{item}</li>
+                        })
+                    }
+                </ul>
+            </Fragment>
+        )
+    }
+
 }
 
 export default App;
---
+++ src/view/Todolist.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Todolist extends Component{
+    render() {
+        return (
+        <div>
+            todolist
+            <p>
+                list
+            </p>
+        </div>
+        )
+        }
+    }
+export default Todolist
