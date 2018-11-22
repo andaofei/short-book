84c935f34ecd9e74560fb836de4a3546441b643e
zhuaofei <2353536563@qq.com>
2018-11-22 23:17:19 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        // this.handleChange = this.handleChange.bind(this)
+        this.state = {
+            inputValue: '1111'
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        <input type="text" onChange={this.handleInputChange.bind(this)} value={this.state.inputValue}/>
+                    </p>
+                </header>
+            </div>
+        );
+    }
+
+    handleInputChange(e) {
+        console.log(e.target.value)
+        this.setState({
+            inputValue: e.target.value
+        })
+        console.log(this.state.inputValue)
+    }
 }
 
 export default App;
